204cbb1c67c4d9a5441d39cde88b490ef0b3a63c
Fabricio-VB08 <f4bricio.vilas.boas@gmail.com>
2025-11-03 07:44:35 -0300

    diferindo a tela de login

--- home/home.php
+++ home/home.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 // Se não estiver logado, redireciona para o login
-if (!isset($_SESSION['id_usuario'])) { header('Location: /TCC-LEGITIMO/cadastroElogin/login.php'); exit; }
+if (!isset($_SESSION['id_usuario'])) {
+    header('Location: /TCC-LEGITIMO/cadastroElogin/login.php');
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +12,7 @@ if (!isset($_SESSION['id_usuario'])) { header('Location: /TCC-LEGITIMO/cadastroE
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Início</title>
+    <link rel="stylesheet" href="home.css">
 </head>
 <body>
 
@@ -19,44 +23,49 @@ if (!isset($_SESSION['id_usuario'])) { header('Location: /TCC-LEGITIMO/cadastroE
    
     <div class="direita">
     <?php if (isset($_SESSION['tipo_usuario']) && $_SESSION['tipo_usuario'] == 'administrador'): ?>
-        <a href="/TCC-LEGITIMO/cadastroElogin/crud_usuarios.php">Gerenciar Usuários</a>
-        
-        <?php endif; ?>
-        <a href="/TCC-LEGITIMO/cadastroElogin/login.php">Sair</a>    
+        <a href="/TCC-LEGITIMO/cadastroElogin/crud_usuarios.php">Gerenciar Usuários</a>    
+    <?php endif; ?>
+        <a href="/TCC-LEGITIMO/cadastroElogin/login.php">Sair</a>
     </div>
 
 
    </div>
 
 <!--  -->
-
     <div class="bodyartificial">
         <div class="container-botao">
             <p class="bemvindo">Bem vindo!</p>
             <div class="horizonte-botao">
-            <button class="botao-picao"  onclick="window.location.href = '/TCC-LEGITIMO/agenda/agenda.php'">
-                <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconcalendario.png">
-                <h3>Ver/Editar Calendário</h3>
-            </button>
+                <?php if ($_SESSION['tipo_usuario'] == 'administrador'): ?>
+                    <!-- Botões para Administrador -->
+                    <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/agenda/agenda.php'">
+                        <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconcalendario.png">
+                        <h3>Ver/Editar Calendário</h3>
+                    </button>
 
-            <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/formulario/index.php'">
-                <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconprofessores.png">
-                <h3>Ver/Cadastrar Professores</h3>
-            </button>
- 
-               <button class="botao-picao"  onclick="window.location.href = '/TCC-LEGITIMO/formulario/crud_competencias.php' ">
-                 <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconmaterias.png">
-                 <h3>Gerenciar Matérias</h3>
-             </button>
+                    <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/formulario/index.php'">
+                        <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconprofessores.png">
+                        <h3>Competências dos Professores</h3>
+                    </button>
+     
+                    <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/formulario/crud_competencias.php'">
+                        <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconmaterias.png">
+                        <h3>Gerenciar Matérias</h3>
+                    </button>
 
-             <button class="botao-picao"  onclick="window.location.href = '/TCC-LEGITIMO/formulario/relatorio_professores.php' ">
-                         <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/relat.png">
-                <h3>Relatório de Professores</h3>
-            </button>
-            </div>
-            <div class="horizonte-botao" style="margin-top: 20px;">
-                
+                    <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/formulario/relatorio_professores.php'">
+                        <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/relat.png">
+                        <h3>Relatório de Professores</h3>
+                    </button>
+                <?php else: ?>
+                    <!-- Botão para Professor -->
+                    <button class="botao-picao" onclick="window.location.href = '/TCC-LEGITIMO/agenda/agenda.php'">
+                        <img class="icone-botao" src="/TCC-LEGITIMO/home/icones/iconcalendario.png">
+                        <h3>Visualizar Calendário</h3>
+                    </button>
+                <?php endif; ?>
             </div>
         </div>
     </div>
-    <link rel="stylesheet" href="home.css">
+</body>
+</html>
