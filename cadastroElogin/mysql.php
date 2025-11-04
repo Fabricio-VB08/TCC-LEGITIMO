81e460c2bccd0783b8b1ac8c93f98deb16f2a686
Fabrício Vilas Boas <f4bricio.vilas.boas@gmail.com>
2025-11-04 08:01:15 -0300

    Add mysql.php for database connection setup

---
+++ cadastroElogin/mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+// Configurações de conexão
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "sistema_senai";
+
+// 1. Conectar ao banco de dados
+$conn = new mysqli($servername, $username, $password, $database);
